19bdd2876ec016edce6191b14e8c2bef67586d35
Akhilesh Upadhyay <umapati4381@gmail.com>
2023-10-29 15:43:42 +0530

    configured cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-import "dotenv/config";//.env
+import "dotenv/config"; //.env
 
 import { createError, errorHandler } from "./middlewares/errorHandling.js";
 import authRoutes from "./routes/auth.js";
@@ -13,14 +13,28 @@ import "./config/db.js";
 import { verifyToken } from "./middlewares/verifyToken.js";
 
 // for using path
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Now you can use __dirname as you normally would in CommonJS modules.
+
+const corsOptions = {
+  origin: [
+    "https://youtube-clone-beta-seven.vercel.app",
+    "https://youtube-clone-git-main-akhileshu.vercel.app",
+    "https://youtube-clone-9ytxa8h8z-akhileshu.vercel.app",
+    "https://youtube-clone-akhileshu.vercel.app",
+    "http://localhost:3000", //for react
+    "http://localhost:4000", //for api
+  ],
+
+  credentials: true, // Allow credentials (cookies)
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
 // server build folder
@@ -28,14 +42,14 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 // Enable extended URL-encoded data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()); //diable when proxy is set in dev mode
+app.use(cors(corsOptions)); //diable when proxy is set in dev mode
 // Allow requests from a specific origin (e.g., your Vercel-hosted frontend)
 // app.use(cors({
 //   origin: 'https://your-vercel-app.vercel.app',
 // }));
 app.use(cookieParser());
 
-// routes 
+// routes
 
 // i have removed the /api prefix because we set then in proxy in react buit it's not set in build folder now
 app.use("/auth", authRoutes);
@@ -44,12 +58,10 @@ app.use("/video", videoRoutes);
 app.use("/comment", commentRoutes);
 
 app.get("*", (req, res) => {
-    // incase user reloads page
+  // incase user reloads page
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 // error handler
 app.use(errorHandler);
-
-
 
 app.listen(4000, () => console.log("server started at port 4000"));
